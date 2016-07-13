3f49673023483350126e13f13c4ef86aea6b6d5c
ohjeung <bioman332@naver.com>
2016-07-13 07:59:20 +0000

    업다운 스크립트

--- app/assets/javascripts/js/script2.js
+++ app/assets/javascripts/js/script2.js
@@ -1,15 +1,6 @@
 window.onload = function() {
     
-     (function(d, c, j) {
-        if (!document.getElementById(j)) {
-            var pd = d.createElement(c),
-                s;
-            pd.id = j;
-            pd.src = ('https:' == document.location.protocol) ? 'https://polldaddy.com/js/rating/rating.js' : 'http://i0.poll.fm/js/rating/rating.js';
-            s = document.getElementsByTagName(c)[0];
-            s.parentNode.insertBefore(pd, s);
-        }
-    }(document, 'script', 'pd-rating-js'));
+    
     //------------------------------------------별
     PDRTJS_settings_8402200 = {
         "id": "8402200",
@@ -217,3 +208,14 @@ window.onload = function() {
 
    
 }
+
+ (function(d, c, j) {
+        if (!document.getElementById(j)) {
+            var pd = d.createElement(c),
+                s;
+            pd.id = j;
+            pd.src = ('https:' == document.location.protocol) ? 'https://polldaddy.com/js/rating/rating.js' : 'http://i0.poll.fm/js/rating/rating.js';
+            s = document.getElementsByTagName(c)[0];
+            s.parentNode.insertBefore(pd, s);
+        }
+    }(document, 'script', 'pd-rating-js'));
