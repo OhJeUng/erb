46240d95d02e55bfa2b66117bde0111435b83c12
ohjeung <bioman332@naver.com>
2016-07-13 07:46:22 +0000

    레이아웃 자바스크립트 추카

--- app/assets/javascripts/js/script2.js
+++ app/assets/javascripts/js/script2.js
@@ -1,4 +1,15 @@
 window.onload = function() {
+    
+     (function(d, c, j) {
+        if (!document.getElementById(j)) {
+            var pd = d.createElement(c),
+                s;
+            pd.id = j;
+            pd.src = ('https:' == document.location.protocol) ? 'https://polldaddy.com/js/rating/rating.js' : 'http://i0.poll.fm/js/rating/rating.js';
+            s = document.getElementsByTagName(c)[0];
+            s.parentNode.insertBefore(pd, s);
+        }
+    }(document, 'script', 'pd-rating-js'));
     //------------------------------------------별
     PDRTJS_settings_8402200 = {
         "id": "8402200",
@@ -204,14 +215,5 @@ window.onload = function() {
 
 
 
-    (function(d, c, j) {
-        if (!document.getElementById(j)) {
-            var pd = d.createElement(c),
-                s;
-            pd.id = j; //이철승 바보색히
-            pd.src = ('https:' == document.location.protocol) ? 'https://polldaddy.com/js/rating/rating.js' : 'http://i0.poll.fm/js/rating/rating.js';
-            s = document.getElementsByTagName(c)[0];
-            s.parentNode.insertBefore(pd, s);
-        }
-    }(document, 'script', 'pd-rating-js'));
+   
 }
